e4b2ad0a3273013f48e7ee4782b110d80821fa59
Remi-Debu <remi.debu@free.fr>
2022-12-21 09:43:52 +0100

    first commit

---
+++ src/test/java/com/localib/LocalibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
